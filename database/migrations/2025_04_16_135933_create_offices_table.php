6deddc816d536ff9641dc94a62d7fda174dee3ad
MohamedHamouch <med10hamouch@gmail.com>
2025-04-16 15:30:16 +0100

    add address column to offices table

--- database/migrations/2025_04_16_135933_create_offices_table.php
+++ database/migrations/2025_04_16_135933_create_offices_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,6 +13,7 @@ return new class extends Migration
         Schema::create('offices', function (Blueprint $table) {
             $table->id();
             $table->foreignId('doctor_id')->constrained('users')->onDelete('cascade');
+            $table->string('address');
             $table->string('office_phone');
             $table->string('specialization');
             $table->decimal('consultation_fee', 8, 2);
