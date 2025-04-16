bc0191ba29c24e4f4cd2626daee72c57982c26a4
MohamedHamouch <med10hamouch@gmail.com>
2025-04-16 23:59:41 +0100

    doctor profiles migration

--- database/migrations/2025_04_16_225841_create_doctor_profiles_table.php
+++ database/migrations/2025_04_16_225841_create_doctor_profiles_table.php
@@ -4,18 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('offices', function (Blueprint $table) {
+        Schema::create('doctor_profiles', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('doctor_id')->constrained('users')->onDelete('cascade');
-            $table->string('address');
-            $table->string('office_phone');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('specialization');
+            $table->text('bio')->nullable();
+            $table->string('office_address')->nullable();
+            $table->string('office_phone')->nullable();
             $table->decimal('consultation_fee', 8, 2);
             $table->timestamps();
         });
@@ -26,6 +28,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('offices');
+        Schema::dropIfExists('doctor_profiles');
     }
 };
