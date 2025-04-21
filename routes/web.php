dbac61e0aa8ec29179d86525c59afcaef5a92a37
MohamedHamouch <med10hamouch@gmail.com>
2025-04-21 11:33:26 +0100

    set home page and auth pages routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+Route::post('/register', [AuthController::class, 'register'])->name('register.submit');
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
+
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/dashboard-client', function () {
+    return view('client.dashboard');
+})->name('client.dashboard');
+
+Route::get('/dashboard-doctor', function () {
+    return view('doctor.dashboard');
+})->name('doctor.dashboard');
+
+Route::get('/dashboard-admin', function () {
+    return view('admin.dashboard');
+})->name('admin.dashboard');
