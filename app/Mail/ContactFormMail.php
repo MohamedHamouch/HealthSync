46e93eabf7aaf97b8c3a7cc15dd8bf95794389ae
Mohamed Hamouch <med10hamouch@gmail.com>
2025-05-04 01:39:10 +0100

    configure contact mail

---
+++ app/Mail/ContactFormMail.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class ContactFormMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * The contact form data.
+     */
+    public $data;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: $this->data['subject'],
+            replyTo: $this->data['email'],
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.contact',
+        );
+    }
+} 
